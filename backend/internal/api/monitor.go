8d666b28c49324b245a50e6484aea3f4f415b597
devbymarcos <marcoslopes.dev@gmail.com>
2025-09-09 22:53:18 -0300

    feat: criando monitor que recebe requisição

--- backend/internal/api/monitor.go
+++ backend/internal/api/monitor.go
@@ -1,10 +1,43 @@
 package api
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
-func HelloHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w, "Olá do pacote Api 🚀")
+func MonitorHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var data interface{}
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		http.Error(w, "JSON invalido", http.StatusBadRequest)
+		return
+	}
+
+	dataDir := filepath.Join(".", "Data")
+	if _, err := os.Stat(dataDir); os.IsNotExist(err) {
+		os.Mkdir(dataDir, os.ModePerm)
+	}
+
+	filePath := filepath.Join(dataDir, "data.json")
+	jsonBytes, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		http.Error(w, "Erro ao processar JSON", http.StatusInternalServerError)
+		return
+	}
+	err = os.WriteFile(filePath, jsonBytes, 0644)
+	if err != nil {
+		http.Error(w, "Erro ao salvar arquivo", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"status":"sucesso"}`))
 }
